05610e818a35bf7a4363b3c4ee952cecc7c16c9e
RElizabet1 <ElizabetR777@mail.ru>
2020-10-06 19:15:27 +0300

    first

---
+++ Game/Game/Form2.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Game
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Label();
+            this.Finish = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Start.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(28, 33);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(93, 32);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Старт";
+            this.Start.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Finish
+            // 
+            this.Finish.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Finish.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Finish.Location = new System.Drawing.Point(683, 400);
+            this.Finish.Name = "Finish";
+            this.Finish.Size = new System.Drawing.Size(93, 32);
+            this.Finish.TabIndex = 1;
+            this.Finish.Text = "Финиш";
+            this.Finish.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Finish.MouseEnter += new System.EventHandler(this.Finish_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 450);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(789, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 450);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(147, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 32);
+            this.label3.TabIndex = 4;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(799, 10);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label5.Font = new System.Drawing.Font("Vivaldi", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(0, 439);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(799, 10);
+            this.label5.TabIndex = 6;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Finish);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Shown += new System.EventHandler(this.Form2_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Start;
+        private System.Windows.Forms.Label Finish;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Game/Game/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void start()
+        {
+            Point point = Start.Location;
+            point.Offset(Start.Width / 2, Start.Height / 2);
+            Cursor.Position = PointToScreen(point);
+        }
+
+        private void Form2_Shown(object sender, EventArgs e)
+        {
+            start();
+        }
+
+
+        private void finishGame()
+        {
+            DialogResult dr = MessageBox.Show("Пам пам", "Message", MessageBoxButtons.YesNo);
+                if (dr == System.Windows.Forms.DialogResult.Yes)
+                start();
+            else
+                DialogResult = System.Windows.Forms.DialogResult.Abort;
+    
+        }
+
+
+        private void Finish_MouseEnter(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            finishGame();
+        }
+    }
+
+
+}
